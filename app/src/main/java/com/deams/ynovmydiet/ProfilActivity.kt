20e3448c4099da8655241c9024d31ff37a8a4320
Jonathan XERRI <jxerri145@gmail.com>
2019-05-23 17:31:24 +0200

    Création page profil avec menu

--- app/src/main/java/com/deams/ynovmydiet/ProfilActivity.kt
+++ app/src/main/java/com/deams/ynovmydiet/ProfilActivity.kt
@@ -1,12 +1,19 @@
 package com.deams.ynovmydiet
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.room.Room
-import kotlinx.android.synthetic.main.activity_profil.*
+import com.google.android.material.navigation.NavigationView
 
-class ProfilActivity : AppCompatActivity() {
+
+class ProfilActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +24,43 @@ class ProfilActivity : AppCompatActivity() {
 
         for (m in users){
             Log.i("Nom", m.name)
-            textView7.text = m.lastname
             Log.i("Prenom", m.lastname)
         }
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_menu_type -> {
+
+            }
+            R.id.nav_mon_suivi -> {
+
+            }
+            R.id.nav_mon_profil -> {
+
+            }
+            R.id.nav_mes_journees -> {
+
+            }
+            R.id.nav_message -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
