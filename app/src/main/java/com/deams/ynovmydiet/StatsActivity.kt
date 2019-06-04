e220c654349a8a743f87936f98569e50857342fc
Damien Rabellino <damien@cyberfab.fr>
2019-06-04 15:40:55 +0200

    Ajout des éléments de Nav et visuel pour StatsActivity

--- app/src/main/java/com/deams/ynovmydiet/StatsActivity.kt
+++ app/src/main/java/com/deams/ynovmydiet/StatsActivity.kt
@@ -11,10 +11,59 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_stats_content.*
 
-class StatsActivity : AppCompatActivity() {
+class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
+
+
+
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_accueil -> {
+                // Handle the camera action
+                val intent1 = Intent(this@StatsActivity, MainActivity::class.java)
+                startActivity(intent1)
+            }
+            R.id.nav_menu_type -> {
+                // Handle the camera action
+                val intent1 = Intent(this@StatsActivity, MenuTypeActivity::class.java)
+                startActivity(intent1)
+            }
+            R.id.nav_mon_profil -> {
+                val intent2 = Intent(this@StatsActivity, ProfilActivity::class.java)
+                startActivity(intent2)
+            }
+            R.id.nav_mes_journees -> {
+                val intent2 = Intent(this@StatsActivity, MaJourneeActivity::class.java)
+                startActivity(intent2)
+            }
+            R.id.nav_mon_suivi -> {
+                val intent4 = Intent(this@StatsActivity, StatsActivity::class.java)
+                startActivity(intent4)
+            }
+            R.id.nav_message -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
