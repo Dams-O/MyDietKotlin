b8f53337a88b64c6bf81c830fb51acf909f6a941
Dams <damien.rabellino@ynov.com>
2020-05-11 17:47:47 +0200

    Config build avec IOS

---
+++ native/KotlinIOS/KotlinIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KotlinIOS
+//
+//  Created by Damien on 04/05/2020.
+//  Copyright © 2020 Damien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
